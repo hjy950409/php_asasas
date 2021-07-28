26c31a6753051b92b33bef96c19fd5dceadd91c5
hjy950409 <hjy950409@naver.com>
2021-07-28 11:48:50 +0900

    add안해서 다시 저장

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>홈</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.7/tailwind.min.css">
+    <link rel="stylesheet" href="common.css">
+    <script src="common.js"></script>
+</head>
+<body>
+
+<div class="site-wrap min-h-screen flex flex-col">
+  <header class="top-bar h-20 flex-shrink-0 bg-black text-white">
+    <div class="container h-full mx-auto px-2 flex">
+      <a href="#" class="logo flex items-center">로고</a>
+      
+      <div class="flex-grow"></div>
+      
+      <ul class="flex -mr-2">
+        <li><a href="/" class="h-full flex items-center px-2 text-red-500 font-bold" href="#">홈</a></li>
+        <li><a href="/pf.php" class="h-full flex items-center px-2" href="#">포트폴리오</a></li>
+        <li><a href="/aboutMe.php" class="h-full flex items-center px-2" href="#">자기소개</a></li>
+        <li><a href="https://www.naver.com/" target="_blank" class="h-full flex items-center px-2" href="#">유튜브</a></li>
+      </ul>
+    </div>
+  </header>
+  <main class="flex-grow flex items-center justify-center">
+    <h1 class="text-4xl">
+      홈
+    </h1>
+  </main>
+  <footer class="bottom-bar h-20 flex-shrink-0 flex justify-center items-center bg-black text-white">
+    <a href="#" class="logo">로고</a>
+  </footer>
+</div>
+</body>
+</html>
